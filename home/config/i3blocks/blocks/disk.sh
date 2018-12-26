ef6d8583e8ce80cb9297f5a0b61ad4f6f158d298
Lilly Chalupowski <lillypadgirl86@gmail.com>
2018-12-25 23:46:05 -0400

    pull

--- home/config/i3blocks/blocks/disk.sh
+++ home/config/i3blocks/blocks/disk.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-df | sed 's/ \+/,/g' | grep root | head -1 | cut -d, -f3 | tr -d '\n'; printf ' MB/'; df --block-size=MB | sed 's/ \+/,/g' | grep root | head -1 | cut -d, -f4 | tr -d '\n'; printf ' MB'
+df --block-size=GB | sed 's/ \+/,/g' | grep root | head -1 | cut -d, -f3 | tr -d '\n'; printf '/'; df --block-size=GB | sed 's/ \+/,/g' | grep root | head -1 | cut -d, -f4 | tr -d '\n'; printf ' GB'
--- home/config/i3blocks/blocks/mem.sh
+++ home/config/i3blocks/blocks/mem.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-free --mega | sed 's/ \+ /,/g' | cut -d, -f3 | sed -n '2p' | tr -d '\n' && printf ' MB/' && free --mega | sed 's/ \+ /,/g' | cut -d, -f2 | sed -n '2p' | tr -d '\n'; printf ' MB'
+free --mega | sed 's/ \+ /,/g' | cut -d, -f3 | sed -n '2p' | tr -d '\n' && printf '/' && free --mega | sed 's/ \+ /,/g' | cut -d, -f2 | sed -n '2p' | tr -d '\n'; printf ' MB'
