b1cb977d13c71cfae0ad4e04b63a0019c747e073
Lilly Chalupowski <lillypadgirl86@gmail.com>
2018-12-26 10:26:01 -0400

    pull

--- home/config/i3blocks/blocks/now_playing.sh
+++ home/config/i3blocks/blocks/now_playing.sh
@@ -3,7 +3,7 @@
 SONG=$(mpc status | head -1 | grep -Pv '^volume')
 
 if [ "$SONG" == "" ]; then
-	echo -n -e "\xef\x96\x8f";
+	echo -n -e "\xef\x96\x8f:";
 else
 	echo -n -e "\xef\x96\x8f: ${SONG}";
 fi
