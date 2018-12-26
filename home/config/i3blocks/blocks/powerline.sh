8beba9f3e5f7cce0fe5b16689c2ec083ee479448
Lilly Chalupowski <lillypadgirl86@gmail.com>
2018-12-26 08:57:25 -0400

    pull

---
+++ home/config/i3blocks/blocks/powerline.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+FOREGROUND=$1
+BACKGROUND=$2
+
+echo -e "<span color='$FOREGROUND' bgcolor='$BACKGROUND'>\xee\x82\xb2</span><span color='$BACKGROUND' bgcolor='$FOREGROUND'>$3</span>"
