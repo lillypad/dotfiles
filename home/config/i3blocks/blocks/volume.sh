1d12c04c3a87e436efad06eba32ea95feb31979e
Lilly Chalupowski <lillypadgirl86@gmail.com>
2019-01-31 20:14:34 -0400

    bug fixes

--- home/config/i3blocks/blocks/volume.sh
+++ home/config/i3blocks/blocks/volume.sh
@@ -2,6 +2,10 @@
 
 VOLUME=$(pacmd list-sinks | pcregrep -a -M '(?<=\*\sindex\:\s)(.+\s+){9}' | grep -Po '(?<=front-left\:\s\d{5}\s\/)\s+\d{1,}' | sed 's/\s//g')
 
+if [ "$VOLUME" = "" ]; then
+	echo -n -e "\xef\x80\xa6:"
+fi
+
 if [ "$VOLUME" -gt "50" ]; then
 	echo -n -e "\xef\x80\xa8: ${VOLUME}%"
 fi
