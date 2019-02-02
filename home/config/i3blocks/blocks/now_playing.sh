f8291a1fa1ca748ac391edf810214295f902aa18
Lilly Chalupowski <lillypadgirl86@gmail.com>
2019-02-02 19:50:25 -0400

    i3blocks

--- home/config/i3blocks/blocks/now_playing.sh
+++ home/config/i3blocks/blocks/now_playing.sh
@@ -5,5 +5,5 @@ SONG=$(mpc status | head -1 | grep -Pv '^volume')
 if [ "$SONG" == "" ]; then
 	echo -n -e "\xef\x96\x8f:";
 else
-	echo -n -e "\xef\x96\x8f: ${SONG}";
+	echo -n -e "\xef\x96\x8f: ${SONG}" | sed 's/\&/\&amp;/g';
 fi
--- home/config/i3blocks/blocks/volume.sh
+++ home/config/i3blocks/blocks/volume.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
-VOLUME=$(pacmd list-sinks | pcregrep -a -M '(?<=\*\sindex\:\s)(.+\s+){9}' | grep -Po '(?<=front-left\:\s\d{5}\s\/)\s+\d{1,}' | sed 's/\s//g')
+VOLUME=$(pacmd list-sinks | pcregrep -a -M '(?<=\*\sindex\:\s)(.+\s+){9}' | grep -Po '(front-left\:\s+\d{1,}\s+\/\s+\d{1,3}\%)' | grep -Po '\d{1,3}(?=\%)')
 
-if [ "$VOLUME" = "" ]; then
-	echo -n -e "\xef\x80\xa6:"
+if [[ -z "$VOLUME" || "$VOLUME" -le "0" ]]; then
+	echo -n -e "<span color='red'>\xef\x80\xa6: mute</span>"
+	exit 0
 fi
 
 if [ "$VOLUME" -gt "50" ]; then
 	echo -n -e "\xef\x80\xa8: ${VOLUME}%"
+	exit 0
 fi
 
-if [[ "$VOLUME" -lt "50"  && "$VOLUME" > "0" ]]; then
+if [[ "$VOLUME" -le "50"  && "$VOLUME" > "0" ]]; then
 	echo -n -e "\xef\x80\xa7: ${VOLUME}%"
+	exit 0
 fi
 
 if [ "$VOLUME" -le "0" ]; then
 	echo -n -e "\xef\x80\xa6: ${VOLUME}%"
+	exit 0
 fi
