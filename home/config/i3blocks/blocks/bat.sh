8049e64152267bcac8ec41d69abc6c7ef8e948c6
lillypad <lillypadgirl86@gmail.com>
2019-05-15 21:12:38 -0300

    update

--- home/config/i3blocks/blocks/bat.sh
+++ home/config/i3blocks/blocks/bat.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CHARGING=$(cat /sys/class/power_supply/AC/online)
 CAPACITY=0
 COUNT=0
 
@@ -10,23 +11,22 @@ done <<<$(find /sys/class/power_supply/ -name "BAT*")
 
 CAPACITY=$(($CAPACITY/$COUNT))
 
-
-if (( CAPACITY > 75 )); then
-	echo -e "\xef\x89\x80|${CAPACITY}%";
-fi
-
-if (( CAPACITY > 50 && CAPACITY <= 75 )); then
-	echo -e "\xef\x89\x81|${CAPACITY}%"
-fi
-
-if (( CAPACITY > 25 && CAPACITY <= 50 )); then
-	echo -e "\xef\x89\x82|${CAPACITY}%"
-fi
-
-if (( CAPACITY > 10 && CAPACITY <= 25 )); then
-	echo -e "\xef\x89\x83|${CAPACITY}%"
-fi
-
-if (( CAPACITY > 0 && CAPACITY <= 10 )); then
-	echo -e "\xEF\x89\x84|${CAPACITY}%";
+if (( CHARGING == 1 )); then
+	echo -e "\xef\x87\xa6|${CAPACITY}%"
+else
+	if (( CAPACITY > 75 )); then
+		echo -e "\xef\x89\x80|${CAPACITY}%";
+	fi
+	if (( CAPACITY > 50 && CAPACITY <= 75 )); then
+		echo -e "\xef\x89\x81|${CAPACITY}%"
+	fi
+	if (( CAPACITY > 25 && CAPACITY <= 50 )); then
+		echo -e "\xef\x89\x82|${CAPACITY}%"
+	fi
+	if (( CAPACITY > 10 && CAPACITY <= 25 )); then
+		echo -e "\xef\x89\x83|${CAPACITY}%"
+	fi
+	if (( CAPACITY > 0 && CAPACITY <= 10 )); then
+		echo -e "\xEF\x89\x84|${CAPACITY}%";
+	fi
 fi
